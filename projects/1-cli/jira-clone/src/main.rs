db73980a3db77f7fa4fe34fb93084aa3368d95de
khinch <khinch-github@nylonmoon.com>
2025-05-26 19:04:02 +0100

    Add Stage 1 Step 3 of Intermediate CLI

---
+++ projects/1-cli/jira-clone/src/main.rs
@@ -0,0 +1,6 @@
+mod db;
+mod models;
+
+fn main() {
+    println!("Welcome to My-Jira!");
+}
---
+++ projects/1-cli/jira-clone/src/models.rs
@@ -0,0 +1,54 @@
+use std::collections::HashMap;
+use std::clone::Clone;
+use serde::{ Deserialize, Serialize };
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+pub enum Status {
+    Open,
+    InProgress,
+    Resolved,
+    Closed,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+pub struct Epic {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+    pub stories: Vec<u32>,
+}
+
+impl Epic {
+    pub fn new(name: String, description: String) -> Self {
+        Self {
+            name,
+            description,
+            status: Status::Open,
+            stories: Vec::new(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+pub struct Story {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+}
+
+impl Story {
+    pub fn new(name: String, description: String) -> Self {
+        Self {
+            name,
+            description,
+            status: Status::Open,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+pub struct DBState {
+    pub last_item_id: u32,
+    pub epics: HashMap<u32, Epic>,
+    pub stories: HashMap<u32, Story>,
+}
