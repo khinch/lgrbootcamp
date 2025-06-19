704d8b4a92ae5280ba9bac120a72f231c8577b62
khinch <khinch-github@nylonmoon.com>
2025-06-19 16:28:46 +0100

    Complete TODOs for Stage 1, Step 1 and 2

--- projects/2-api/stackoverflow-clone/src/handlers/mod.rs
+++ projects/2-api/stackoverflow-clone/src/handlers/mod.rs
@@ -1,33 +1,46 @@
 use crate::models::*;
 use axum::{Json, response::IntoResponse};
 
-// ---- CRUD for Questions ----
-
 pub async fn create_question(Json(question): Json<Question>) -> impl IntoResponse {
-    todo!()
+    Json(QuestionDetail {
+        question_uuid: "question_uuid".to_owned(),
+        title: "title".to_owned(),
+        description: "description".to_owned(),
+        created_at: "created_at".to_owned(),
+    })
 }
 
 pub async fn read_questions() -> impl IntoResponse {
-    todo!()
+    Json(vec![QuestionDetail {
+        question_uuid: "question_uuid".to_owned(),
+        title: "title".to_owned(),
+        description: "description".to_owned(),
+        created_at: "created_at".to_owned(),
+    }])
 }
 
 pub async fn delete_question(Json(question_uuid): Json<QuestionId>) {
-    todo!()
+    ()
 }
 
-// ---- CRUD for Answers ----
+pub async fn create_answer(Json(answer): Json<Answer>) -> impl IntoResponse {
+    Json(AnswerDetail {
+        answer_uuid: "answer_uuid".to_owned(),
+        question_uuid: "question_uuid".to_owned(),
+        content: "content".to_owned(),
+        created_at: "created_at".to_owned(),
+    })
+}
 
-// TODO: Create a POST route to /answer which accepts an `Answer` and returns `AnswerDetail` as JSON.
-//       The handler function should be called `create_answer`.
-//
-//       hint: this function should look very similar to the create_question function above
+pub async fn read_answers() -> impl IntoResponse {
+    Json(vec![AnswerDetail {
+        answer_uuid: "answer_uuid".to_owned(),
+        question_uuid: "question_uuid".to_owned(),
+        content: "content".to_owned(),
+        created_at: "created_at".to_owned(),
+    }])
+}
 
-// TODO: Create a GET route to /answers which accepts an `QuestionId` and returns a vector of `AnswerDetail` as JSON.
-//       The handler function should be called `read_answers`.
-//
-//       hint: this function should look very similar to the read_questions function above
-
-// TODO: Create a DELETE route to /answer which accepts an `AnswerId` and does not return anything.
-//       The handler function should be called `delete_answer`.
-//
-//       hint: this function should look very similar to the delete_question function above
+pub async fn delete_answer(Json(answer_uuid): Json<AnswerId>) {
+    ()
+}
