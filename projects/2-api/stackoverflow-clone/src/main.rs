dcc19bc106241ecfac142e4f5df32a912d21b498
khinch <khinch-github@nylonmoon.com>
2025-06-20 15:13:01 +0100

    Complete TODOs for Stage 2 Step 2

--- projects/2-api/stackoverflow-clone/src/main.rs
+++ projects/2-api/stackoverflow-clone/src/main.rs
@@ -1,16 +1,20 @@
-// TODO: import log, pretty_env_logger, dotenv, and PgPoolOptions
+#[macro_use]
+extern crate log;
+
+extern crate pretty_env_logger;
+
 use axum::{
     Router,
     routing::{delete, get, post},
 };
+
 use dotenvy::dotenv;
-use log::{debug, error, info, trace, warn};
-use models::Question;
+
 use sqlx::postgres::PgPoolOptions;
-use std::env;
 
 mod handlers;
 mod models;
+mod persistance;
 
 use handlers::*;
 
@@ -19,32 +23,21 @@ async fn main() {
     pretty_env_logger::init();
     dotenv().ok();
 
-    // Create a new PgPoolOptions instance with a maximum of 5 connections.
-    // Use dotenv to get the database url.
-    // Use the `unwrap` or `expect` method instead of handling errors. If an
-    // error occurs at this stage the server should be terminated.
-    // See examples on GitHub page: https://github.com/launchbadge/sqlx
-    // let pool = todo!();
-    //
-    let db_url = env::var("DATABASE_URL").unwrap();
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect(&db_url)
+        .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set."))
         .await
-        .unwrap();
+        .expect("Failed to create Postgres connection pool!");
 
-    // Using slqx, execute a SQL query that selects all questions from the questions table.
-    // Use the `unwrap` or `expect` method to handle errors. This is just some test code to
-    // make sure we can connect to the database.
-    // let recs = todo!();
-    let recs = sqlx::query("SELECT * FROM questions")
+    // TODO: Delete this query
+    let recs = sqlx::query!("SELECT * FROM questions")
         .fetch_all(&pool)
         .await
         .unwrap();
 
+    // TODO: Delete these log statements
     info!("********* Question Records *********");
-    // TODO: Log recs with debug formatting using the info! macro
-    info!("recs.len: {}", recs.len());
+    info!("{:?}", recs);
 
     let app = Router::new()
         .route("/question", post(create_question))
